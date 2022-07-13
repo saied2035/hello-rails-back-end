d26449353bba25fc65215a4095831d0c1aa1c1be
Saied Gaffer <saied2421998@gmail.com>
2022-07-13 09:55:20 +0200

    add greetings to seed.rb

--- db/migrate/20220712084832_create_messages.rb
+++ db/migrate/20220712084832_create_messages.rb
@@ -4,10 +4,5 @@ class CreateMessages < ActiveRecord::Migration[7.0]
       t.string :data
       t.timestamps
     end
-    Message.create(data:'Greeting 1')
-    Message.create(data:'Greeting 2')
-    Message.create(data:'Greeting 3')
-    Message.create(data:'Greeting 4')
-    Message.create(data:'Greeting 5')
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+    Message.create(data:'Greeting 1')
+    Message.create(data:'Greeting 2')
+    Message.create(data:'Greeting 3')
+    Message.create(data:'Greeting 4')
+    Message.create(data:'Greeting 5')
